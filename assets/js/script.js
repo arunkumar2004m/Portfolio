0449b52e67ea6023cc31a431d911b435f1a2025d
arunkumar2004m <arunvishal217@gmail.com>
2025-03-23 22:17:05 +0530

    some changes and added new file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
 // JavaScript for Mobile Menu Toggle
-
 document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".toggle");
     const sidebar = document.querySelector(".sidebar");
     const closeButton = document.querySelector(".x");
     const menuLinks = document.querySelectorAll(".menubarLinks a"); // Select all menu links
-
     // Open sidebar when clicking the menu button
     menuToggle.addEventListener("click", function () {
       sidebar.style.left = "0"; // Open sidebar
     });
-
     // Close sidebar when clicking the 'X' button
     closeButton.addEventListener("click", function () {
       sidebar.style.left = "-60%"; // Close sidebar
     });
-
     // Close sidebar when clicking a menu item
     menuLinks.forEach((link) => {
       link.addEventListener("click", function () {
@@ -23,32 +19,18 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-
-
 function about() {
     document.getElementById("about").scrollIntoView({ behavior: "smooth" });
 }
-
 function project() {
     document.getElementById("project").scrollIntoView({ behavior: "smooth" });
 }
-
 function skill() {
     document.getElementById("skill").scrollIntoView({ behavior: "smooth" });
-
 }
-
 function contact() {
     document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
 }
-
-
-
-
-
 const project1 = (url) => window.open(url, "_blank"); // Opens URL in a new tab
 const project2 = (url) => (window.location.href = url); // Opens URL in a new tab
 const project3 = (url) => window.open(url, "_blank"); // Opens URL in a new tab
-
-
-
