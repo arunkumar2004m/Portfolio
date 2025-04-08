b132eb629a213d840e1a6a61ea541b58efad3cd2
arunkumar2004m <arunvishal217@gmail.com>
2025-04-08 14:54:33 +0530

    refresh remove

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -13,24 +13,4 @@ else {
 }
 
 
-let touchStartY = 0;
 
-document.addEventListener("touchstart", function (event) {
-    touchStartY = event.touches[0].clientY;
-});
-
-document.addEventListener("touchend", function (event) {
-    let touchEndY = event.changedTouches[0].clientY;
-    if (touchEndY - touchStartY > 100) { // Swipe down detected
-        location.reload(); // Refresh the page
-    }
-});
-
-
-document.addEventListener("touchmove", function(event) {
-    event.preventDefault(); // This prevents pull-to-refresh, remove it to enable
-}, { passive: false });
-
-
-        
-      
